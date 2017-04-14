05fb32ed53d94e7c2171f3188cfb981194da8cbd
Developer Davo <developerdavo@gmail.com>
2017-04-14 18:52:57 +0200

    Refactor into object literal and separate functions

--- js/index.js
+++ js/index.js
@@ -1,24 +1,29 @@
-function generateRandomQuote() {
-    var quotes = [
+var randomQuoteGenerator = {
+    quotes: [
         {text: "Life is about making an impact, not making an income.", person: "Kevin Kruse"},
         {text: "Whatever the mind of man can conceive and believe, it can achieve.", person: "Napoleon Hill"},
         {text: "Strive not to be a success, but rather to be of value.", person: "Albert Einstein"},
         {text: "Two roads diverged in a wood, and I—I took the one less traveled by, And that has made all the difference.", person: "Robert Frost"},
         {text: "I attribute my success to this: I never gave or took any excuse.", person: "Florence Nightingale"}
-    ];
-
-    var index = Math.floor(Math.random() * quotes.length);
-    return quotes[index];
-}
-
-window.onload = function() {
-    var button = document.getElementById("button");
-    button.onclick = function() {
-        var element = document.getElementById("quote");
-        var quote = generateRandomQuote();
-        var formattedQuote = quote.text + " - " + quote.person;
-        element.innerHTML = formattedQuote;
-        console.log(formattedQuote);
-
-    };
+    ],
+    generateRandomQuote: function() {
+        var index = Math.floor(Math.random() * this.quotes.length);
+        return this.quotes[index];
+    }
 };
+
+function displayQuote() {
+    var element = document.getElementById("quote");
+    var quote = randomQuoteGenerator.generateRandomQuote();
+    var formattedQuote = quote.text + " - " + quote.person;
+    element.innerHTML = formattedQuote;
+    console.log(formattedQuote);
+};
+
+function onButtonClick() {
+    var button = document.getElementById("button");
+    button.onclick = displayQuote;
+};
+
+window.onload = onButtonClick();
+
