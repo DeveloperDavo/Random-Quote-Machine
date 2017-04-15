ed8591d92662172194f0bd4c24f4d93a11bf116d
Developer Davo <developerdavo@gmail.com>
2017-04-15 07:54:08 +0200

    Refactor into MVC

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-var randomQuoteGenerator = {
+var model = {
     quotes: [
         {text: "Life is about making an impact, not making an income.", person: "Kevin Kruse"},
         {text: "Whatever the mind of man can conceive and believe, it can achieve.", person: "Napoleon Hill"},
@@ -12,16 +12,36 @@ var randomQuoteGenerator = {
     }
 };
 
-function displayQuoteAndAddShareButton() {
-    var quoteElement = document.getElementById("quote");
-    var quote = randomQuoteGenerator.generateRandomQuote();
-    var formattedQuote = quote.text + " - " + quote.person;
-    quoteElement.innerHTML = formattedQuote;
-    var shareButton = document.getElementById("twitter-share-section");
-    shareButton.innerHTML = '<a id="tweet" class="twitter-share-button" href="https://twitter.com/share" data-size="large" data-text="' + formattedQuote + '" data-hashtags="quotes" data-related="twitterapi,twitter">Tweet</a>';
-    twttr.widgets.load();
-};
+var view = {
+    displayQuote: function(formattedQuote) {
+        var quoteElement = document.getElementById("quote");
+        quoteElement.innerHTML = formattedQuote;
+    },
 
+    // http://stackoverflow.com/questions/9215806/how-to-update-the-twitter-share-button-url-with-dynamic-content
+    displayShareButton: function(formattedQuote) {
+        var shareButton = document.getElementById("twitter-share-section");
+        shareButton.innerHTML = '<a id="tweet" class="twitter-share-button" href="https://twitter.com/share" data-size="large" data-text="' + formattedQuote + '" data-hashtags="quotes" data-related="twitterapi,twitter">Tweet</a>';
+    }
+
+}
+
+var controller = {
+    formateQuote: function() {
+        var quote = model.generateRandomQuote();
+        return quote.text + " - " + quote.person;
+    },
+
+    displayFormattedQuoteAndLoadShareButton: function() {
+        var formattedQuote = controller.formateQuote();
+
+        view.displayQuote(formattedQuote);
+        view.displayShareButton(formattedQuote);
+        twttr.widgets.load();
+    }
+}
+
+// https://dev.twitter.com/web/javascript/loading
 window.twttr = (function(d, s, id) {
   var js, fjs = d.getElementsByTagName(s)[0],
     t = window.twttr || {};
@@ -41,7 +61,8 @@ window.twttr = (function(d, s, id) {
 
 window.onload = function() {
     var button = document.getElementById("button");
-    button.onclick = displayQuoteAndAddShareButton;
-    displayQuoteAndAddShareButton();
+    button.onclick = controller.displayFormattedQuoteAndLoadShareButton;
+
+    controller.displayFormattedQuoteAndLoadShareButton();
 }
 
